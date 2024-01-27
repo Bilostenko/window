feb669575c0a5429aeac3c7edebece331fc63b72
Bilostenko <bilostenkod@gmail.com>
2024-01-27 16:53:54 +0200

    add modal

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,34 +1,46 @@
 const modals = () => {
-  function bindModal(triger, modal, close) {
-    triger.addEventListener('click', (e) => {
+  function bindModal(trigerSelector, modalSelector, closeSelector) {
 
-      if (e.target) {
-        e.preventDefault()
-      }
-      modal.style.display = 'block';
-      document.body.style.overflow = 'hidden';
+    const trigger = document.querySelectorAll(trigerSelector)
+    const modal = document.querySelector(modalSelector)
+    const close = document.querySelector(closeSelector)
+
+    trigger.forEach(item => {
+      item.addEventListener('click', (e) => {
+        if (e.target) {
+          e.preventDefault()
+        }
+        modal.style.display = 'block';
+        /* document.body.style.overflow = 'hidden'; */
+        docyment.body.classlist.add('.modal-open')
+      })
     })
 
     close.addEventListener('click', () => {
       modal.style.display = 'none';
-      document.body.style.overflow = '';
+      /* document.body.style.overflow = ''; */
+      docyment.body.classlist.remove('.modal-open')
     })
 
     modal.addEventListener('click', (e) => {
       if (e.target === modal) {
         modal.style.display = "none";
-        document.body.style.overflow = "";
+        /* document.body.style.overflow = ""; */
+        docyment.body.classlist.remove('.modal-open')
       }
     });
-
   }
 
-  const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
-    modalEngineer = document.querySelector('.popup_engineer'),
-    modalEnginnerClose = document.querySelector('.popup_engineer .popup_close');
+  function showModalByTime(selector, time){
+    setTimeout(function(){
+      document.querySelector(selector).style.display = 'block'
+      document.body.style.overflow = 'hidden';
+    }. time)
+  }
 
-  bindModal(callEngineerBtn, modalEngineer, modalEnginnerClose)
-
+  bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close')
+  bindModal('.phone_link', '.popup', '.popup .popup_close')
+  bindModal('.popup', 6000)
 }
 
 
